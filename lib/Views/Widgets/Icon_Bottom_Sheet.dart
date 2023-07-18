53ef4023d5b251a0c2a57b05284e966e765d6f9f
ahmed17405 <ahmed17405@gmail.com>
2023-07-18 07:04:47 +0300

    add note bottom

---
+++ lib/Views/Widgets/Icon_Bottom_Sheet.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class IconButtonsheet extends StatelessWidget {
+  const IconButtonsheet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      color: Colors.blue[900],
+        minWidth: double.infinity,
+        height: 60,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+        onPressed: () {},
+        child: Text("ADD"));
+  }
+}
